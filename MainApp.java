d3a15735a1238b51dc18ba6dce6e4b92e190b63e
Krunal32 <krunalpatel33@hotmail.com>
2016-12-02 23:11:05 -0500

    Update MainApp.java

--- MainApp.java
+++ MainApp.java
@@ -3,37 +3,23 @@ import java.util.Scanner;
 
 public class MainApp {
 
-    static Room[] room_list;
+    //static Room[] room_list;
+    static ArrayList<Customer> customer_details;
     static ArrayList<Reservation> reservation_list;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+	ReservationManager rm = new ReservationManager();
+	Customer customer = new Customer();
 	
-	
-		
-	      	room_list = new Room[20];
-	        reservation_list = new ArrayList<Reservation>();
-	        for (int i = 0; i < room_list.length; i++) {
-	            if (i < 5) {
-	                room_list[i] = new Single(100 + i);
-	            }
-	            else if (i < 10) {
-	                room_list[i] = new Double(200 + (i-5));
-	            }
-	            else if (i < 15) {
-	                room_list[i] = new Suite(300+(i-10));
-	            }     
-	        }
-	    
-		
 		
 		while (true) {
 			System.out.println("\nTest menu:");
-			System.out.println("1 - Make new reservation");
-			System.out.println("2 - View/change existing reservation");
-			System.out.println("3 - Check out of hotel");
-			System.out.println("4 - quit");
+			System.out.println("1 - Create a Profile");
+			System.out.println("2 - Create Reservation");
+			System.out.println("3 - Display Currey Reservation");
+			System.out.println("4 - Display Profiles");
 
  			System.out.print("\nEnter choice: ");
 			int choice = IO.readInt();
@@ -41,21 +27,35 @@ public class MainApp {
 			switch (choice) {
 
 				case 1:
-					ReservationManager.makeNewResv(reservation_list);
-					break;
+				rm.getCustomerInfo();
+				break; 
 				
 				case 2:
-					ReservationManager.changeResv(resvs);
+				System.out.println("Enter your email: ");
+				String email =IO.readString();
+				ReservationManager.createReservation(email);
+				break;
+				
+				case 3:
+					rm.showCurrent();
+					break;
+				case 4:
+					rm.showCurrentProfile();
+					break;
+					//System.out.println("Customer added");
+			
+			/*	case 2:
+					ReservationManager.changeResv(reservation_list);
 					break;
 				
 				case 3:
-					ReservationManager.checkOut(resvs);
+					ReservationManager.checkOut(reservation_list);
 					break;
 					
 				case 4:
 					System.out.println("GoodBye!");
 					return;
-					
+			*/		
 			}
 		}
 	        
